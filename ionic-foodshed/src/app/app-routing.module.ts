5b0d5d7454e6d7128da0a506c5f92b6b1fa7488e
Avery2 <JustinAveryChan@gmail.com>
2019-11-19 20:49:01 -0600

    tabs

--- ionic-foodshed/src/app/app-routing.module.ts
+++ ionic-foodshed/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'info', loadChildren: './home/info/info.module#InfoPageModule' },
 ];
 
 @NgModule({
--- ionic-foodshed/src/app/home/home.page.ts
+++ ionic-foodshed/src/app/home/home.page.ts
@@ -1,26 +1,27 @@
 import { Component } from '@angular/core';
 
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })export class HomePage {
-  markers: any[]; 
+  markers: any[];
   map: any;
   constructor() {
     this.markers = []; // TODO: initialize array of markers
   }
-  ionViewDidEnter() { //run when app is opened
+  ionViewDidEnter() { // run when app is opened
     /*Initializing Map*/
+    // tslint:disable-next-line: max-line-length
     mapboxgl.accessToken = 'pk.eyJ1IjoiYWVjaGFuIiwiYSI6ImNqbXd4ZmYzYTA0eWcza3J0NzVsNnNkcWoifQ.PQuBcFIs9STCQ6uf8DrJNw'; // token from previous group
-    this.map = new mapboxgl.Map({ //initialize new map object
+    this.map = new mapboxgl.Map({ // initialize new map object
       style: 'mapbox://styles/aechan/cjmwxodn95lir2rmoq60ydb3m', // style defined by previous group
       center: [-89.4125, 43.0766], // center lat and longitude
       zoom: 15,
       pitch: 0,
-      minZoom: 1, //restrict map zoom - buildings not visible beyond 13
+      minZoom: 1, // restrict map zoom - buildings not visible beyond 13
       maxZoom: 17,
       container: 'map'
     });
